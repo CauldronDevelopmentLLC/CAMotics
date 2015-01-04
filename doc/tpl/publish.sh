b49bae9c17dc93889832d4b3e843bf16bc67dc9e
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-01-04 01:46:50 -0800

    Updated API

--- doc/tpl/publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-TARGET=root@coffland.com:/var/www/tplang.org/http/
-
-rsync -av --exclude=.svn --exclude=publish.sh "$(dirname "$0")"/ $TARGET
--- doc/tpl/tpl.js
+++ doc/tpl/tpl.js
@@ -1,64 +1,53 @@
+function create_sidebar() {
+    var top = $('#sidebar ul');
+    var lastH1;
+    var lastH2;
+
+    $('#content h1, #content h2, #content .func').each(function () {
+        var elem = $(this);
+        var tag = elem.prop('tagName');
+        var id = elem.attr('id');
+
+        switch (tag) {
+        case 'H1':
+            lastH1 = $('<li>')
+                .append($('<a>').attr('href', '#' + id)
+                        .text(elem.text()))
+                .appendTo(top);
+            break;
+
+        case 'H2':
+            var ul = lastH1.find('> ul');
+            if (!ul.length) ul = $('<ul>').appendTo(lastH1);
+
+            lastH2 = $('<li>')
+                .append($('<a>').attr('href', '#' + id)
+                        .text(elem.text()))
+                .appendTo(ul);
+            break;
+
+        case 'DIV':
+            var ul = lastH2.find('> ul');
+            if (!ul.length) ul = $('<ul>').appendTo(lastH2);
+
+            var title = id.replace(/-/g, '.');
+
+            $('<li>')
+                .append($('<a>').attr('href', '#' + id)
+                        .text(title + '()'))
+                .appendTo(ul);
+            break;
+        }
+    });
+}
+
+
 $(function() {
-    var sidebar = $('#sidebar');
-    var content = $('#content');
+    // Open links in new window
+    $('a').attr('target', '_blank');
 
-    // Create sidebar menu & modify sections
-    $.each(content.find('div[id]'), function(i, div) {
-        div = $(div)
-        var id = div.attr('id');
-
-        var title = div.attr('title');
-        if (typeof title == 'undefined') title = id;
-        else if (div.hasClass('L0') || div.hasClass('L1'))
-            div.prepend($('<h2>').addClass('title').text(title));
-        else if (div.hasClass('L2'))
-            div.prepend($('<h3>').addClass('title').text(title));
-
-        div.addClass('level');
-
-        var entry = $('<div><a href="#' + id + '">' + title + '</a></div>');
-        entry.addClass(div.attr('class'));
-        entry.addClass('level');
-
-        sidebar.append(entry);
-    });
-
-
-    // Fix example indentation
-    $.each(content.find('.example'), function(i, example) {
-        example = $(example);
-
-        // Split lines
-        var lines = example.html().match(/([^\r\n]*)\r?\n\r?/g);
-        if (lines == null) return;
-
-        // Compute indent
-        var indent = 0;
-        for (var i = 0; i < lines.length; i++) {
-            lines[i] = lines[i].replace(/\s+$/g, '');
-            if (!lines[i].length) continue;
-
-            var len = lines[i].match(/^\s*/)[0].length;
-            if (!indent || len < indent) indent = len;
-        }
-
-        // Remove indent
-        if (indent)
-            for (var i = 0; i < lines.length; i++)
-                if (indent < lines[i].length)
-                    lines[i] = lines[i].substr(indent);
-
-        // Rejoin lines
-        var html = '';
-        for (var i = 0; i < lines.length; i++) {
-            if (html) html += '\n';
-            html += lines[i];
-        }
-
-        // Reset contents
-        example.html(html);
-    });
-
+    // Create sidebar menu
+    create_sidebar();
 
     // Setup simulation images
     $('.sim-image')
@@ -68,15 +57,7 @@ $(function() {
             $(this).css({width: w == '300px' ? 'auto' : '300px'});
         });
 
-    function colorize(i, div) {
-        var div = $(div);
-        var code = $('<code>')
-            .attr({'data-language': 'javascript'})
-            .append(div.html());
-        div.html(code);
-    }
-
-    $.each(content.find('.example'), colorize);
-    $.each(content.find('.func .decl'), colorize);
+    // Syntax highlight code
+    $('#content .example, #content .decl').attr('data-language', 'javascript');
     Rainbow.color();
 });
