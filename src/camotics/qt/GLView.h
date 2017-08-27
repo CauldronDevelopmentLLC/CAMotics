30f8b0bffcafef7ddf73f8688690f03a556ffa63
Joseph Coffland <joseph@cauldrondevelopment.com>
2017-08-27 01:15:27 -0700

    Attempt to fix Qt version check

--- src/camotics/qt/GLView.h
+++ src/camotics/qt/GLView.h
@@ -22,7 +22,7 @@
 
 
 #include <QtGlobal>
-#if QT_VERSION < QT_VERSION_CHECK(5, 4, 0)
+#if (QT_VERSION < QT_VERSION_CHECK(5, 4, 0))
 #include <QGLWidget>
 #define PLATFORM_GL_WIDGET QGLWidget
 #else
