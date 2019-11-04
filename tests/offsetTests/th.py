9910f585c406c92e19e7a22145f7079aa5519381
Joseph Coffland <joseph@cauldrondevelopment.com>
2019-11-03 22:49:15 -0800

    Updated test system

--- tests/offsetTests/th.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Suite:
-    def __init__(self, th):
-        cmd = os.path.abspath(th.path + '/../../camoeval')
-
-        th.Test('CoordinateSystem', command = cmd)
-        th.Test('Global', command = cmd)
--- tests/th.py
+++
@@ -1,3 +0,0 @@
-class Suite:
-    def __init__(self, th):
-        pass
