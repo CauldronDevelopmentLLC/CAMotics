7d217c632599303bc753365f9bc5ea3472fb1175
Joseph Coffland <joseph@cauldrondevelopment.com>
2019-11-29 00:20:27 -0800

    Fixed GCode output but effecting matrix rotations, OCode test

--- src/gcode/machine/GCodeMachine.cpp
+++ src/gcode/machine/GCodeMachine.cpp
@@ -265,7 +265,7 @@ void GCodeMachine::move(const Axes &_target, int axes, bool rapid) {
     string next = dtos(target.get(*axis), imperial).toString();
 
     // Always output axis the first time
-    if (wasSeen && (!(axes & axisVT) || last == next)) continue;
+    if (wasSeen && last == next) continue;
 
     if (first) {
       beginLine();
