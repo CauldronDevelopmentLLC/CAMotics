fc7a4550e3bf7a0e27d2e1e32f67cb49cf1c2ea7
Joseph Coffland <joseph@cauldrondevelopment.com>
2022-11-18 16:46:14 +0200

    Windows fixes

--- src/clipper/OffsetBuilder.cpp
+++ src/clipper/OffsetBuilder.cpp
@@ -35,8 +35,11 @@
 #include "Util.h"
 #include "Clipper.h"
 
-#define _USE_MATH_DEFINES
 #include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 using namespace ClipperLib;
 
